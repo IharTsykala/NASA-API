b7023d90f9f13517b1e2c827671e3ab42a36f0ff
Ihar Tsykala <Sudja09@bk.ru>
2020-06-18 10:36:01 +0300

    Updates

---
+++ precache-manifest.125ec8bb0421dd101e07ea0ed62a592f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed09cac7e2f2fdd690be9b6cd855455",
+    "url": "/NASA-API/index.html"
+  },
+  {
+    "revision": "c04f3098de8a1ef9d85f",
+    "url": "/NASA-API/static/css/main.a93b66d0.chunk.css"
+  },
+  {
+    "revision": "8b6ec88c0c49b211d24b",
+    "url": "/NASA-API/static/js/2.2cfaaf9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NASA-API/static/js/2.2cfaaf9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04f3098de8a1ef9d85f",
+    "url": "/NASA-API/static/js/main.0291e85c.chunk.js"
+  },
+  {
+    "revision": "32439cded22b87839bf8",
+    "url": "/NASA-API/static/js/runtime-main.b1fa09b7.js"
+  }
+]);
