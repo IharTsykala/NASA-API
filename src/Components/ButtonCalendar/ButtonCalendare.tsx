7509002fcd3ce7bc16c1d9b853acca921ec87cb0
Ihar Tsykala <Sudja09@bk.ru>
2020-06-20 17:07:46 +0300

    feat: add calendar

--- src/Components/ButtonCalendar/ButtonCalendare.tsx
+++ src/Components/ButtonCalendar/ButtonCalendare.tsx
@@ -1,10 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import DateFnsUtils from "@date-io/date-fns"
+import Grid from "@material-ui/core/Grid"
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers"
 import { Box } from "@material-ui/core"
 
 const ButtonCalendar: React.FunctionComponent = () => {
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+    new Date()
+  )
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date)
+  }
   return (
     <Box component={"div"} className={"button-calendar"}>
-      Calendar
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="space-around">
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Date picker inline"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
     </Box>
   )
 }
